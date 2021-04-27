5d1b490915e9b67787b97be2255a8970b1a8877f
alexs3700 <alexs3700@gmail.com>
2021-04-27 10:18:21 +0200

    Delete model.js

--- Oblig2/UnitTesting/model.js
+++
@@ -1,7 +0,0 @@
-
-        let inputValue = 3;
-        let chosenBar = 'id3';
-        let idNum = ['id1','id2','id3','id4','id5'];
-        let myIndex =  idNum.indexOf(chosenBar);
-        let counterArray = [0, 0, 0, 0, 0];
-        let numbers =  [7, 3, 1, 5, 8];
